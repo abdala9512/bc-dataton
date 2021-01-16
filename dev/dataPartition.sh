b18552c52f7ac3ba8394976b9c298cbbf087bc89
abdala9512 <miguel.arquez12@gmail.com>
2021-01-16 00:34:07 -0500

    Data Partition program

--- dev/dataPartition.sh
+++ dev/dataPartition.sh
@@ -1,1 +1,10 @@
-# Data partition
+#!/bin/bash
+
+# Make sure to install csvkit: pip install csvkit
+months=(201902 201903 201904 201905 201907 201908 201909 201910 201911
+        202001 202002 202003 202004 202005 202007 202008 202009 202010 202011)
+
+for month in ${months[*]}
+do
+    csvgrep -H -c 1 -m "$month" Dataton_train.csv > train_$month.csv 
+done 
