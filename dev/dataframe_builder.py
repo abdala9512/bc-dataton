4f60c505c66aa384356b61b23de59ae0d06015c9
abdala9512 <miguel.arquez12@gmail.com>
2021-01-17 11:13:55 -0500

    dataframe header method

--- dev/dataframe_builder.py
+++ dev/dataframe_builder.py
@@ -4,8 +4,15 @@ import pandas as pd
 
 class DataFrameBuilder:
     
+    HEADER="https://bc-dataton2020.s3.amazonaws.com/dataton_all_data/header.txt"
+    
     def __init__(self, dataframe):
-        self.original_dataframe = dataframe
+        self.original_dataframe = self._assign_columns(dataframe)
+        
+    def _assign_columns(self, dataframe):
+        column_names = pd.read_csv(DataFrameBuilder.HEADER).columns.to_list()
+        dataframe.columns = column_names
+        return dataframe
         
     def handle_missing_data(self):
         pass
